912ba169b6ff9035cedd0cd63b8cecaef02e111b
مهرداد <youremail@example.com>
2026-08-02 18:48:33 +0200

    Add fixed debug keystore to fix signature mismatch on every build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,15 @@ android {
     namespace = "com.example.babyai"
     compileSdk = 34
 
+    signingConfigs {
+        getByName("debug") {
+            storeFile = file("debug.keystore")
+            storePassword = "android"
+            keyAlias = "androiddebugkey"
+            keyPassword = "android"
+        }
+    }
+
     defaultConfig {
         applicationId = "com.example.babyai"
         minSdk = 24
